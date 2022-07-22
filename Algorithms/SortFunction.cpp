e939e858f103f121af44c10c96e56b852ce38889
Dhananjay Kumar <64013411+dhananjay229@users.noreply.github.com>
2022-07-22 21:20:41 +0530

    Add files via upload

---
+++ Algorithms/SortFunction.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+bool compare(int a, int b){
+		cout<< "comparing 5 " << a <<" and "<< b <<endl;
+		return a>b;
+	}
+	
+int main(){
+	
+	int n, key;
+	cin>> n;	
+	int a[1000];
+		
+	for(int i=0; i<n; i++){
+		cin >> a[i];
+	}
+	
+	sort(a, a+n, compare);
+	
+	for(int i=0; i<n; i++){
+		cout << a[i] <<",";
+	}
+	
+	return 0;
+	
+}
