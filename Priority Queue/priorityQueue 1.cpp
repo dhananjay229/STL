ceb7d0ea58ba3406e40399e6255470b30b7b8489
Dhananjay Kumar <64013411+dhananjay229@users.noreply.github.com>
2022-08-18 19:32:35 +0530

    Add files via upload

---
+++ Priority Queue/priorityQueue 1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int main(){
+	
+	priority_queue<int> pq;
+	
+	int n;
+	cin>>n;
+	
+	for(int i=1; i<=n; i++){
+		int no;
+		cin>>no;
+		pq.push(no);
+	}
+	
+	while(!pq.empty()){
+		cout<< pq.top()<< " ";
+		pq.pop();
+	}
+	
+	return 0;
+}
