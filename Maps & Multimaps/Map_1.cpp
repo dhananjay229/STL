68fa4ca585b99e9e35be24be638af21f901003a5
Dhananjay Kumar <64013411+dhananjay229@users.noreply.github.com>
2022-08-19 20:21:16 +0530

    Add files via upload

---
+++ Maps & Multimaps/Map_1.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<map>
+#include<cstring>
+
+using namespace std;
+
+int main(){
+	
+	map<string, int> m;
+	
+	//insert 1
+	m.insert(make_pair("Mango",100));
+	
+	//insert 2
+	pair<string,int> p;
+	p.first = "Apple";
+	p.second = 120;
+	m.insert(p);
+	
+	// search
+	string fruit;
+	cin>>fruit;
+	
+	auto it = m.find(fruit);
+	if(it!=m.end()){
+		cout<<"price of "<<fruit<<" is "<<m[fruit]<<endl;
+	}
+	else{
+		cout<<"fruit is not present "<<endl;
+	}
+	
+	//it store unique keys only once
+	if(m.count(fruit)){
+		cout<<"Price is "<<endl;
+	}
+	
+	//Erase
+	m.erase(fruit);
+	
+	//update the price
+	m[fruit] += 20;
+	
+	m["litchi"] = 60;
+	m["pineapple"] = 80;
+	
+	//iterate over all the key value pair
+	for(auto it=m.begin(); it!=m.end(); it++){
+		cout << it->first<<" and "<<it->second<<endl;
+	}
+	
+	
+	//for each loop
+	for(auto p:m){
+		cout<<p.first<< ":"<<p.second<<endl;
+	}
+	
+	
+	
+	
+	
+	return 0;	
+}
+
+
+
+
+
+
+
+
